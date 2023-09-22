402545aab604756a4543a84d7edb41626cbfa2f2
Minooo1 <meiyeyoutai@gmail.com>
2023-09-23 00:25:59 +0900

    chore: cobra cli init

--- main.go
+++ main.go
@@ -1,22 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/spf13/cobra"
-)
-
-var rootCmd = &cobra.Command{
-	Use: "hello-cli",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello, World")
-	},
-}
+import "github.com/Minooo1/hello-cli/cmd"
 
 func main() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
+	cmd.Execute()
 }
