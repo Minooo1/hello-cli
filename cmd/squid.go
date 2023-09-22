37e0edc1d45a14b963cce3f73993bd7a8b8aa65b
Minooo1 <meiyeyoutai@gmail.com>
2023-09-23 01:31:49 +0900

    feat: イカのaa追加

---
+++ cmd/squid.go
@@ -0,0 +1,31 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// squidCmd represents the stegen command
+var squidCmd = &cobra.Command{
+	Use:   "squid",
+	Short: "Print the funny ascii art of squid",
+	Run: func(cmd *cobra.Command, args []string) {
+		b, err := os.ReadFile("askiiArt/squid.txt")
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Print(string(b))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(squidCmd)
+}
